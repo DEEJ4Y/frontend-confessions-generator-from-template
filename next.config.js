dab08682996824883cf4ef8a48f25963f740e6e4
davidjosephind <davidjosephind@gmail.com>
2021-12-22 11:01:36 +0530

    Updated build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   reactStrictMode: true,
-  // assetPrefix: isProd ? "https://deej4y.github.io/socialautopost" : "",
+  basePath: "/socialautopost",
 };
