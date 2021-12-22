f3dcc41e8427fd5fd58e69f39c43d7493ed71f09
davidjosephind <davidjosephind@gmail.com>
2021-12-22 10:55:27 +0530

    Update next config for gh-pages, new build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   reactStrictMode: true,
-}
+  // assetPrefix: isProd ? "https://deej4y.github.io/socialautopost" : "",
+};
